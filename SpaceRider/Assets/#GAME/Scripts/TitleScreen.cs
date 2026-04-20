046c8e739cb8c3dd4fb3b36c5fc5993a7d79e022
GreenCalx <romain.schoenberger@gmail.com>
2026-04-20 20:43:54 +0200

    title

--- SpaceRider/Assets/#GAME/Scripts/TitleScreen.cs
+++ SpaceRider/Assets/#GAME/Scripts/TitleScreen.cs
@@ -1,16 +1,31 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class TitleScreen : MonoBehaviour
 {
     [SerializeField] private string mainSceneName = "main";
 
+    [Header("Input")]
+    [SerializeField] private InputAction startAction = new InputAction("Start", InputActionType.Button);
+
     private bool _loading;
+
+    private void Awake()
+    {
+        EnsureBinding(startAction, "<Keyboard>/space");
+        EnsureBinding(startAction, "<Keyboard>/enter");
+        EnsureBinding(startAction, "<Gamepad>/start");
+        EnsureBinding(startAction, "<Gamepad>/buttonSouth");
+    }
+
+    private void OnEnable()  => startAction.Enable();
+    private void OnDisable() => startAction.Disable();
 
     private void Update()
     {
         if (_loading) return;
-        if (Input.anyKeyDown)
+        if (startAction.WasPressedThisFrame())
             Load();
     }
 
@@ -18,5 +33,12 @@ public class TitleScreen : MonoBehaviour
     {
         _loading = true;
         SceneManager.LoadScene(mainSceneName);
+    }
+
+    private static void EnsureBinding(InputAction action, string path)
+    {
+        foreach (var b in action.bindings)
+            if (b.path == path) return;
+        action.AddBinding(path);
     }
 }
